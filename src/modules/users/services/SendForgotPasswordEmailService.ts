80cb20b1d0d7f223c6d6db66d7a13f9df5767629
William Ferreira Nunes <willianfn@msn.com>
2020-09-07 01:25:02 -0300

    change forgot password url format

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -61,7 +61,7 @@ export default class SendForgotPasswordEmailService {
                 variables: {
                     nome: user.name,
                     email: user.email,
-                    link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+                    link: `${process.env.APP_WEB_URL}/reset-password?token=${token}`,
                 },
             },
         });
